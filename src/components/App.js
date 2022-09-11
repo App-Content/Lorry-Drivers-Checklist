5b51e4a089e02b01ff196936506818c222e62e52
Krzysztof Feret <krzysztof.feret@appcontent.pl>
2022-09-11 17:12:00 +0200

    Set up the project environment

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./styles/App.scss";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div className="App">Do sprawdzenia przed wyjazdem</div>;
+  }
+}
+
+export default App;
