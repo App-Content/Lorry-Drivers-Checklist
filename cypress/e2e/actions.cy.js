5b74bfbb7abdfd817be03ce7c91b1f32fd0873ce
Krzysztof Feret <krzysztof.feret@appcontent.pl>
2022-09-24 11:56:50 +0200

    Update cypress sample tests

--- cypress/e2e/actions.cy.js
+++ cypress/e2e/actions.cy.js
@@ -11,4 +11,35 @@ describe("launch home page", () => {
       .invoke("css", "display")
       .should("equal", "block");
   });
+
+  it("Click TableToDo", () => {
+    cy.get(":nth-child(1) > :nth-child(2) > button")
+      .click()
+      .get(".todoList__header > span")
+      .contains(15)
+      .get("[data-test-id=toDoItems]")
+      .find("tr")
+      .should("have.length", 15);
+
+    cy.get(":nth-child(7) > :nth-child(3) > input")
+      .click()
+      .get("[data-test-id=toDoItems]")
+      .find("tr")
+      .should("have.length", 14)
+      .get(".doneList__header > span")
+      .contains(1)
+      .get("[data-test-id=doneItems]")
+      .find("tr")
+      .should("have.length", 1)
+      .get(".doneList > .table > tbody > tr > :nth-child(1)")
+      .contains("Karta kierowcy");
+  });
+
+  it("Form input using", () => {
+    cy.get(".form__input").type("Nowa");
+    cy.get(".form__button")
+      .click()
+      .get(":nth-child(17) > :nth-child(1)")
+      .contains("Nowa");
+  });
 });
